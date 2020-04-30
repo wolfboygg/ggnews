972526246564680acf48cd9ab7dfac08d49dc786
guolei <guolei@do-global.com>
2020-04-30 21:34:56 +0800

    change[]:使用DataBinding绑定数据

--- app/src/main/java/com/ggwolf/ggnews/MainActivity.java
+++ app/src/main/java/com/ggwolf/ggnews/MainActivity.java
@@ -1,14 +1,20 @@
 package com.ggwolf.ggnews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.ggwolf.ggnews.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mainBinding.executePendingBindings();
     }
 }
